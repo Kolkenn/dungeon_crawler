328a78c7947fdf52ca08d66a75c85ea27d9e2876
Kolkenn <leonel.ponce96@gmail.com>
2026-03-16 22:12:40 -0400

    Refactor monster spawning logic in Dungeon class to use GENERAL_MONSTER_TYPES and BOSS_MONSTERS

--- src/dungeon.py
+++ src/dungeon.py
@@ -1,6 +1,6 @@
 import random
 from src.player import Player
-from src.monster import MONSTER_TYPES
+from src.monster import GENERAL_MONSTER_TYPES,BOSS_MONSTERS
 from src.combat import run_combat
 
 TOTAL_FLOORS = 5 # Total number of floors in the dungeon
@@ -23,11 +23,19 @@ class Dungeon:
         monster = self._spawn_monster()
         run_combat(player, monster)
 
+        # TODO: add loot drops, gold rewards, etc.
+        # TODO: add choice of furthering deeper or leave and save progress (if we implement saving/loading) after each encounter, especially if player is low on health or resources. Maybe also add a shop encounter every few floors where player can buy healing potions, gear upgrades, etc.
+
         if player.is_alive():
             self.floor += 1
 
     def _spawn_monster(self):
         """Randomly selects a monster type and creates an instance of it."""
-        monster_types = random.choice(list(MONSTER_TYPES.values()))
-        monster_func = random.choice(monster_types)
-        return monster_func()
+        if self.floor == TOTAL_FLOORS: # Spawn a boss on the final floor
+            boss_types = random.choice(list(BOSS_MONSTERS.values()))
+            boss_func = random.choice(boss_types)
+            return boss_func()
+        else: # Spawn a regular monster on other floors
+            monster_types = random.choice(list(GENERAL_MONSTER_TYPES.values()))
+            monster_func = random.choice(monster_types)
+            return monster_func()
